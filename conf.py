d862512f445ba4a53829b1670780edeb25989dce
keyestudio <215650023@qq.com>
2025-02-05 11:09:14 +0800

    2025-02-05 11:09:14.18

--- conf.py
+++ conf.py
@@ -17,17 +17,20 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'keyestudio WiKi'
-copyright = 'keyestudio , WiKi'
-author = 'keyestudio WiKi'
-copyright = 'Shenzhen keyestudio Technology Co., Ltd'
+project = 'keyes WiKi'
+copyright = 'keyes , WiKi'
+author = 'keyes WiKi'
+copyright = 'Shenzhen keyes Technology Co., Ltd'
+
+root_doc = "index"
+master_doc = "index"
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = ['myst_parser','sphinx_copybutton']
 
 source_suffix = {
     '.rst': 'restructuredtext',
@@ -65,7 +68,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+  "show_toc_level": 3,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
